bc1e9a8dcd6716657fe3df1210007ac23f97ea97
Yixuan Wen <weny8@uw.edu>
2021-04-11 21:30:58 +0800

    first commit

---
+++ app/src/main/java/com/example/dotify/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.dotify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun nextClicked(view:View){
+        Log.i("nc", "Next has been clicked")
+
+        Toast.makeText(this, "Skipping to next track", Toast.LENGTH_SHORT).show()
+    }
+    fun prevClicked(view:View){
+        Log.i("nc", "Next has been clicked")
+
+        Toast.makeText(this, "Skipping to previous track", Toast.LENGTH_SHORT).show()
+    }
+}
